5c7b68168889f338f75f9e88baa2434cd07251ff
Karl Schoch <karl.schoch@gmail.com>
2025-05-31 14:22:55 -0400

    Successfuly implements hamburger menu that initiates dropdown for larger menu

--- js/include-all-pages.js
+++ js/include-all-pages.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     fetch("/partials/siteNav.html")
         .then(res => res.text())
-        .then(
-            data => {
-                document.getElementById("site-nav").innerHTML = data
+        .then(html => {
+            document.getElementById("site-nav").innerHTML = html;
+            if (window.initNavToggle) {
+                window.initNavToggle();
+            }
         });
 });
--- js/nav-menu-toggle.js
+++ js/nav-menu-toggle.js
@@ -1,6 +1,8 @@
 // Responsive nav toggle
-const toggle = document.querySelector('.nav-toggle');
-const links = document.querySelector('.nav-links');
-toggle.addEventListener('click', () => {
-    links.classList.toggle('active');
-});
+window.initNavToggle = function() {
+    const toggle = document.querySelector('.nav-toggle');
+    const links = document.querySelector('.nav-links');
+    toggle.addEventListener('click', () => {
+        links.classList.toggle('active');
+    });
+}
